d7e62428c6da54ddb2e51e6c58836f1f71d1152d
Nico <nicolaspalacio91@gmail.com>
2018-05-09 09:54:34 +0200

    Renamed utils.py to strings.py
    Added license statement.

--- strings.py
+++ strings.py
@@ -1,3 +1,25 @@
+# -*- coding: utf-8 -*-
+
+'''
+Data tools - String operations module
+
+Copyright (C) 2018 Nicolàs Palacio
+
+Contact: nicolaspalacio91@gmail.com
+
+GNU-GLPv3:
+This program is free software: you can redistribute it and/or modify it under
+the terms of the GNU General Public License as published by the Free Software
+Foundation.
+
+This program is distributed in the hope that it will be useful, but WITHOUT ANY
+WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+A full copy of the GNU General Public License can be found on file
+"LICENSE.md". If not, see <http://www.gnu.org/licenses/>.
+'''
+
 __all__ = ['is_numeric', 'join_str_lists']
 
 def is_numeric(s):
