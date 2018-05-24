e3313680757cb8acdd5680cc4df1aaee8c45adee
Nico <nicolaspalacio91@gmail.com>
2018-05-24 16:59:00 +0200

    Added piano_consensus plotting function

--- src/data_tools/plots.py
+++ src/data_tools/plots.py
@@ -7,9 +7,11 @@ data_tools.plots
 Plotting functions module.
 '''
 
-__all__ = ['volcano']
+__all__ = ['volcano', 'piano_consensus']
 
 import numpy as np
+import pandas as pd
+import matplotlib
 import matplotlib.pyplot as plt
 
 def volcano(logfc, logpval, thr_pval=0.05, thr_fc=2., c=('C0', 'C1'),
@@ -98,6 +100,90 @@ def volcano(logfc, logpval, thr_pval=0.05, thr_fc=2., c=('C0', 'C1'),
     if legend:
         ax.legend()
 
+    fig.tight_layout()
+
+    if filename:
+        fig.savefig(filename)
+
+    return fig
+
+def piano_consensus(df, nchar=40, boxes=True, title=None, filename=None,
+                    figsize=None):
+    '''
+    Generates a GSEA consensus score plot like R package ``piano``'s
+    ``consensusScores`` function, but prettier.
+    The main input is assumed to be a ``pandas.DataFrame`` whose data
+    is the same as the ``rankMat`` from the result of
+    ``consensusScores``.
+
+    * Arguments:
+        - *df* [pandas.DataFrame]: Values contained correspond to the
+          scores of the gene-sets (consensus and each individual
+          methods). Index must contain the gene-set labels. Columns are
+          assumed to be ``ConsRank`` (ignored), ``ConsScore`` followed
+          by the individual methods (e.g.: ``mean``, ``median``,
+          ``sum``, etc).
+        - *nchar* [int]: Optional, ``40`` by default. Number of string
+          characters of the gene-set labels of the plot.
+        - *boxes* [bool]: Optional, ``True`` by default. Determines
+          whether to show the boxplots of the gene-sets or not.
+        - *title* [str]: Optional, ``None`` by default. Defines the plot
+          title.
+        - *filename* [str]: Optional, ``None`` by default. If passed,
+          indicates the file name or path where to store the figure.
+          Format must be specified (e.g.: .png, .pdf, etc)
+        - *figsize* [tuple]: Optional, ``None`` by default (default
+          matplotlib size). Any iterable containing two values denoting
+          the figure size (in inches) as [width, height].
+
+    * Returns:
+        - [*matplotlib.figure.Figure*]: the figure object containing a
+          combination of box and scatter plots of the gene-set scores.
+
+    * Examples:
+        >>> piano_consensus(df, figsize=[7, 8])
+
+        .. image:: ../figures/piano_consensus_example.png
+    '''
+
+    # List of equidistant colors according to a colormap
+    cmap = matplotlib.cm.get_cmap('rainbow')
+    colors = map(cmap, np.linspace(1, 0, len(df.columns[2:])))
+
+    y = range(len(df))[::-1]
+
+    fig, ax = plt.subplots(figsize=figsize)
+
+    # Gene-set scores for each individual method
+    for i, col in enumerate(df.columns[2:]):
+        ax.scatter(df[col], y, c=colors[i], alpha=.5, label=col)
+
+    # Box plot of the gene-sets
+    if boxes:
+        ax.boxplot(df.iloc[:, 2:], positions=y, widths=.75, vert=False,
+                   zorder=1, medianprops={'linewidth':0},
+                   flierprops={'markersize':7})
+
+    # Consensus score across methods (substitutes the median of the
+    # boxplot for visibility)
+    col = 'ConsScore'
+    ax.scatter(df[col], y, label=col, c='r', zorder=2, s=94,
+               marker=[(-.1, 1), (.1, 1),
+                       (.1, -1), (-.1, -1)])
+
+    # Gene-set labels
+    ax.set_yticks(y)
+    ax.set_yticklabels([s[:nchar] for s in df.index])
+
+    # Axes properties
+    ax.set_xlabel('Score')
+    ax.set_ylabel('Gene-set', rotation=90, ha='center')
+    ax.set_ylim(-1, len(df))
+
+    if title:
+        ax.set_title(title)
+
+    ax.legend(loc=0)
     fig.tight_layout()
 
     if filename:
