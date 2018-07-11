ff0576d6e4568d393d9c53d5ed1a8fd57f37debc
Nico <nicolaspalacio91@gmail.com>
2018-07-11 13:34:02 +0200

    Updated setup.py and Sphinx's conf.py
    Now author and version are automatically taken from __init__.py

--- setup.py
+++ setup.py
@@ -1,16 +1,18 @@
 # -*- coding: utf-8 -*-
 
-from setuptools import setup, find_packages
+from setuptools import setup #, find_packages
+
+from data_tools import __version__, __author__
 
 setup(name='data_tools',
-      version='0.0.4',
+      version=__version__,
       description="Data tools: a collection of Python functions and classes designed to make data scientists' life easier.",
       url='https://github.com/Nic-Nic/data_tools',
-      author='Nicolàs Palacio',
+      author=__author__,
       author_email='nicolaspalacio91@gmail.com',
       license='GNU-GLPv3',
       package_dir = {'':'src'},
-      packages=find_packages('src'),
+      packages=['data_tools'], #find_packages('src'),
       install_requires=['numpy',
                         'matplotlib',
                         'pandas',
