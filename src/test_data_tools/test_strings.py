02bf8cd8b2a13bb38d64a0934fee0de0c41f5792
NP <nicolaspalacio91@gmail.com>
2018-07-18 21:07:55 +0200

    Added tests for strings module

--- src/test_data_tools/test_strings.py
+++ src/test_data_tools/test_strings.py
@@ -1,1 +1,41 @@
 # -*- coding: utf-8 -*-
+
+__all__ = ['IsNumericTestCase', 'JoinStrLists']
+
+import unittest
+
+from data_tools import strings
+
+
+class IsNumericTestCase(unittest.TestCase):
+    def test_int(self):
+        self.assertTrue(strings.is_numeric('1'))
+        self.assertTrue(strings.is_numeric('-1'))
+
+    def test_float(self):
+        self.assertTrue(strings.is_numeric('1.0'))
+        self.assertTrue(strings.is_numeric('-1.0'))
+
+    def test_nan(self):
+        self.assertTrue(strings.is_numeric('NaN'))
+
+    def test_nonum(self):
+        self.assertFalse(strings.is_numeric('Hello world'))
+
+
+class JoinStrLists(unittest.TestCase):
+    def test_same_size(self):
+        self.assertEqual(strings.join_str_lists(['a', 'b'], ['1', '2']),
+                         ['a1', 'b2'])
+
+    def test_diff_size(self):
+        self.assertEqual(strings.join_str_lists(['a', 'b'], ['1', '2', '3']),
+                         ['a1', 'b2'])
+
+    def test_sep(self):
+        self.assertEqual(strings.join_str_lists(['a', 'b'], ['1', '2'], sep='_'),
+                         ['a_1', 'b_2'])
+
+    def test_non_str(self):
+        with self.assertRaises(TypeError):
+            strings.join_str_lists(['a', 'b'], [1, 2])
