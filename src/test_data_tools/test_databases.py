df834ccac50b9878189f1b12ea564b8259a9466e
Nico <nicolaspalacio91@gmail.com>
2018-07-27 10:04:57 +0200

    Added test for databases.kegg_link

--- src/test_data_tools/test_databases.py
+++ src/test_data_tools/test_databases.py
@@ -1,12 +1,26 @@
 # -*- coding: utf-8 -*-
 
-__all__ = ['UpQueryTestCase']
+__all__ = ['KeggLinkTestCase', 'UpQueryTestCase']
 
 import unittest
 
 import pandas as pd
 
 from data_tools import databases
+
+class KeggLinkTestCase(unittest.TestCase):
+    def setUp(self):
+        self.result = databases.kegg_link(['hsa:10458', 'ece:Z5100'])
+        self.expected = pd.DataFrame([['hsa:10458', 'path:hsa04520'],
+                                      ['hsa:10458', 'path:hsa04810'],
+                                      ['ece:Z5100', 'path:ece05130']],
+                                     columns=['query', 'pathway'])
+
+    def test_instance(self):
+        self.assertIsInstance(self.result, pd.DataFrame)
+
+    def test_content(self):
+        pd.testing.assert_frame_equal(self.result, self.expected)
 
 class UpQueryTestCase(unittest.TestCase):
     def setUp(self):
