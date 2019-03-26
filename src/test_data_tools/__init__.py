5899f3158abe3f71b204a3479c766d2c5f5ecc3b
Nico <nicolaspalacio91@gmail.com>
2019-03-26 17:43:58 +0100

    Improved testing suite

--- src/test_data_tools/__init__.py
+++ src/test_data_tools/__init__.py
@@ -1,2 +1,9 @@
 # -*- coding: utf-8 -*-
 #!/usr/bin/env python
+
+import test_data_tools.test_databases as test_databases
+import test_data_tools.test_diffusion as test_diffusion
+import test_data_tools.test_iterables as test_iterables
+import test_data_tools.test_models as test_models
+import test_data_tools.test_plots as test_plots
+import test_data_tools.test_strings as test_strings
--- src/test_data_tools/__main__.py
+++ src/test_data_tools/__main__.py
@@ -1,13 +1,45 @@
 # -*- coding: utf-8 -*-
 #!/usr/bin/env python
 
+import os
+import time
 import unittest
 
 import test_data_tools
 
 
 if __name__ == '__main__':
-    test_loader = unittest.TestLoader()
-    test_suite = test_loader.discover(test_data_tools.__path__[0])
+    start = time.time()
 
-    unittest.TextTestRunner(verbosity=2).run(test_suite)
+    print '\nWelcome to data_tools test suite'
+    print '################################\n'
+    print 'Session started @ %s' % time.strftime('%a %d %b %Y - %H:%M')
+
+    test_loader = unittest.TestLoader()
+    run = []
+    skip = []
+    fail = []
+
+    for submod in [m for m in dir(test_data_tools) if m.startswith('test_')]:
+        msg = 'Running tests for submodule %s' % submod.replace('test_', '')
+        print '\n%s\n%s' % (msg, '=' * len(msg))
+
+        test_suite = test_loader.loadTestsFromModule(getattr(test_data_tools,
+                                                             submod))
+        res = unittest.TextTestRunner(verbosity=2).run(test_suite)
+        run.append(res.testsRun)
+        skip.append(len(res.skipped))
+        fail.append(len(res.errors))
+
+    if sum(fail) == 0:
+        msg = 'OK'
+
+    else:
+        msg = 'FAILED (errors=%d)' % sum(fail)
+
+    if sum(skip) != 0:
+        msg += ' (skipped=%d)' % sum(skip)
+
+    print '=' * 70
+    print 'TOTAL ran %d tests in %.3fs\n' % (sum(run), time.time() - start)
+    print msg
