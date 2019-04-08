a3c722213450ecb6c56992da74e0e544ac415ac5
Nico <nicolaspalacio91@gmail.com>
2019-04-08 16:49:33 +0200

    Created test case class (empty) for gauss_kernel

--- src/test_data_tools/test_signal.py
+++ src/test_data_tools/test_signal.py
@@ -12,3 +12,10 @@ class FConvolveTestCase(unittest.TestCase):
                    + 'mplemented.')
     def test_null(self):
         pass
+
+
+class GaussKernelTestCase(unittest.TestCase):
+    @unittest.skip('** NOTE **: data_tools.signal.gauss_kernel test uni'
+                   + 't is not implemented.')
+    def test_null(self):
+        pass
