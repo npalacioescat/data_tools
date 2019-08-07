33748401346e0d65a34c69bb0ed7b9751fec612d
Nico <nicolaspalacio91@gmail.com>
2019-08-07 16:41:55 +0200

    Added PowerLawTestCase

--- src/test_data_tools/test_models.py
+++ src/test_data_tools/test_models.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
-__all__ = ['DoseResponseTestCase', 'LassoTestCase', 'LinearTestCase']
+__all__ = ['DoseResponseTestCase', 'LassoTestCase', 'LinearTestCase',
+           'PowerLawTestCase']
 
 import unittest
 
@@ -33,6 +34,7 @@ class DoseResponseTestCase(unittest.TestCase):
     def test_ec(self):
         self.assertEqual(round(1 - self.model.ec() / self.params[0], 0), 0)
 
+
 class LassoTestCase(unittest.TestCase):
     @unittest.skip('** NOTE **: data_tools.models.Lasso test unit is not yet'
                    + ' implemented.')
@@ -53,3 +55,18 @@ class LinearTestCase(unittest.TestCase):
 
     def test_slope(self):
         self.assertEqual(self.model.slope, 1.0)
+
+
+class PowerLawTestCase(unittest.TestCase):
+    def setUp(self):
+        # Fit y = x^-2
+        self.x = [1, 2, 4]
+        self.y = [1, 0.25, 0.0625]
+
+        self.model = models.PowerLaw(self.x, self.y)
+
+    def test_exponent(self):
+        self.assertEqual(self.model.k, -2.0)
+
+    def test_const(self):
+        self.assertEqual(self.model.a, 1.0)
